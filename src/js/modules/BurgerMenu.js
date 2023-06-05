dc778ce101952bb43df07bb1aa65b1ff1be7ee5b
Daniil Usmantsev <daniil.usmantsev@gmail.com>
2023-06-05 16:10:06 +0300

    update berger clousing

--- src/js/modules/BurgerMenu.js
+++ src/js/modules/BurgerMenu.js
@@ -1,5 +1,6 @@
 class BurgerMenu {
   constructor() {
+    this.body = document.body;
     this.burgerButton = document.querySelector('.burger-button');
     this.burgerMenu = document.querySelector('.burger-menu');
     this.burgerCross = document.getElementById('burger-cross');
@@ -9,15 +10,26 @@ class BurgerMenu {
   init() {
     if (this.burgerButton) {
       this.burgerButton.addEventListener('click', () => {
-        this.burgerMenu.children[0].classList.add('menu-open');
+        this.burgerMenu.children[1].classList.add('menu-open');
         this.burgerMenu.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
         this.rootElement.style.pointerEvents = 'none';
+        this.burgerMenu.style.pointerEvents = 'all';
+      });
+      this.burgerMenu.addEventListener('click', (event) => {
+        console.info(event.target);
+        if (event.target.classList.contains('burger-menu__left')) {
+          this.burgerMenu.children[1].classList.remove('menu-open');
+          this.burgerMenu.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+          this.rootElement.style.pointerEvents = 'all';
+          this.burgerMenu.style.pointerEvents = 'none';
+        }
       });
       if (this.burgerCross) {
         this.burgerCross.addEventListener('click', () => {
-          this.burgerMenu.children[0].classList.remove('menu-open');
+          this.burgerMenu.children[1].classList.remove('menu-open');
           this.burgerMenu.style.backgroundColor = 'rgba(0, 0, 0, 0)';
           this.rootElement.style.pointerEvents = 'all';
+          this.burgerMenu.style.pointerEvents = 'none';
         });
       }
     }
